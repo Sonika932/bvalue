5c64effbab97f92cf5729b16a96fda53f55eb9bb
sonika932 <sonikasingh932@gmail.com>
2021-02-22 19:47:17 +0530

    second commit

--- b_value_using_countfunction.py
+++ b_value_using_countfunction.py
@@ -4,14 +4,9 @@ import matplotlib.pyplot as plt
 from pandas import ExcelFile
 m= pd.read_excel(r'C:\Users\DELL\Desktop\b value\data.xlsx')
 print(m)
-n= len(m)
 earthquake=[]
-j= 0.1
-for j in range(0.1,10.0, 0.1):
-    number = 0
-    for i in range (0,n):
-        if m[i]== j:
-            number= number+1
+for j in np.arange(0.1,10.0, 0.1):
+    number= m.count(j)
     earthquake.append(number)
     print('number of earthquake at mag', j,' is: ', number)
 print(earthquake)
