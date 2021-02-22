fae554aeec6d9f955a2f3cf71917d43cd0f341c7
sonika932 <sonikasingh932@gmail.com>
2021-02-22 16:50:02 +0530

    first commit

--- bvalue.py
+++ bvalue.py
@@ -1,11 +1,16 @@
-m=[1,1,1,1,2,2,2,2,2,2,5,5,5,3,3,7,7]
-n= len(m)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import ExcelFile
+m= pd.read_excel(r'C:\Users\DELL\Desktop\b value\data.xlsx')
+print(m)
+n= float(len(m))
 earthquake=[]
-for j in range(1,7):
-    number = 0
-    for i in range (0,n):
+for j in range(0.1,10.0, 0.1):
+    number = 0.0
+    for i in range (0.0,n):
         if m[i]== j:
-            number= number+1
+            number= number+1.0
     earthquake.append(number)
     print('number of earthquake at mag', j,' is: ', number)
 print(earthquake)
